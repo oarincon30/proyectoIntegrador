b8e9750d7be7eb5f2ae1f43d79f240ea014a8982
oarincon30 <oarincon30@ucatolica.edu.co>
2022-10-05 11:38:40 -0500

    primer commit

---
+++ src/main/java/com/proyectoIntegrador/market/MarketPiApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoIntegrador.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketPiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectoIntegrador/market/MarketPiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoIntegrador.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
